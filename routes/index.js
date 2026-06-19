d631d21dc3183806009fb8a229b690cbdbd5f9d1
Ravi Hiremath <hiremathravi333@gmail.com>
2026-06-20 00:06:01 +0530

    case-insensitive search in mongoose

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+const userModel = require("./users");
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index');
 });
 
-router.get('/failed', function(req, res) {
-  req.flash("age" ,12);
-  req.flash("name" ,"harsh");
-  res.send("bangaya");
+router.get('/create',async function(req, res) {
+let userdata = await userModel.create({
+   username : "harshi",
+  nickname : "harshiiiiii",
+  description : "hello everyone",
+  categories : ['fashion','life','science'],
+ })
+ res.send(userdata);
 });
 
-router.get('/checkkaro', function(req, res) {
-  console.log(req.flash("age"),req.flash("name"));
-  res.send("check karo be ke terminal par");
+
+router.get('/find',async function(req, res) {
+  var regex = new RegExp("^harshIta$",'i');
+  let user = await userModel.findOne({username: regex});
+  res.send(user);
 });
+
+
 
 module.exports = router;
