de933408185ef624dad3103ff612837b9908a867
Ravi Hiremath <hiremathravi333@gmail.com>
2026-06-20 00:11:56 +0530

    RegExp

--- routes/index.js
+++ routes/index.js
@@ -20,8 +20,8 @@ let userdata = await userModel.create({
 
 
 router.get('/find',async function(req, res) {
-  var regex = new RegExp("^harSh$",'i');
-  let user = await userModel.find({username: regex});
+  var regex = new RegExp("^hArsh$",'i');
+  let user = await userModel.findOne({username: regex});
   res.send(user);
 });
 
