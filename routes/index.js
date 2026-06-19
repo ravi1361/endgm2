14e07d891192f5dcbbccb50ef17cf17be955137a
Ravi Hiremath <hiremathravi333@gmail.com>
2026-06-20 00:10:07 +0530

    using Regular Expression(RegExp) in mongoose

--- routes/index.js
+++ routes/index.js
@@ -20,7 +20,7 @@ let userdata = await userModel.create({
 
 
 router.get('/find',async function(req, res) {
-  var regex = new RegExp("^harshIta$",'i');
+  var regex = new RegExp("^harSh$",'i');
   let user = await userModel.find({username: regex});
   res.send(user);
 });
