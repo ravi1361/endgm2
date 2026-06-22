86336f98e2b14cb6f4dfd826fecf110629d6253f
Ravi Hiremath <hiremathravi333@gmail.com>
2026-06-22 18:21:13 +0530

    Authentication and Authorization

--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,61 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 const userModel = require("./users");
+const passport = require("passport");
+const localStrategy = require("passport-local");
+
+passport.use(new localStrategy(userModel.authenticate()));
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index');
+router.get("/", function (req, res) {
+  res.render("index");
 });
 
-router.get('/create',async function(req, res) {
-let userdata = await userModel.create({
-   username : "harshi",
-  nickname : "harshiiiiii",
-  description : "hello everyone",
-  categories : ['fashion','life','science'],
- })
- res.send(userdata);
+router.get("/profile", isLoggedIn, function (req, res) {
+  res.render("profile");
 });
 
-
-router.get('/find',async function(req, res) {
-  let user = await userModel.find({
-    $expr: {
-      $and : [
-        {$gte: [{$strLenCP:'$nickname'},13]},
-        {$lte: [{$strLenCP:'$nickname'},122]} 
-      ]
-    }
+//register route
+router.post("/register", function (req, res) {
+  var userdata = new userModel({
+    username: req.body.username,
+    secret: req.body.secret,
   });
-  res.send(user);
+
+  userModel
+    .register(userdata, req.body.password)
+    .then(function (registereduser) {
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/profile");
+      });
+    });
 });
 
+// code for login
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/profile",
+    failureRedirect: "/",
+  }),
+  function (req, res) {},
+);
+
+//code for logout
+router.get("/logout", function (req, res, next) {
+  req.logout(function (err) {
+    if (err) return next(err);
+    res.redirect("/");
+  });
+});
+
+//code for isLoggedIn middleware
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/");
+}
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,29 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
+// const passportLocalMongoose = require("passport-local-mongoose");
 
+// mongoose.connect("mongodb://127.0.0.1:27017/testingendgame2");
+
+// const userSchema = mongoose.Schema({
+//   username: String,
+//   password: String,
+//   secret: String,
+// });
+
+// userSchema.plugin(passportLocalMongoose);
+
+// module.exports = mongoose.model("user", userSchema);
+
+const mongoose = require("mongoose");
+const plm = require("passport-local-mongoose").default;
+console.log(typeof plm);
 mongoose.connect("mongodb://127.0.0.1:27017/testingendgame2");
 
-const userSchema = mongoose.Schema({
-  username : String,
-  nickname : String,
-  description : String,
-  categories : {
-    type : Array,
-    default: []
-  },
-  datecreated : {
-    type : Date,
-    default: Date.now()
-  }
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  secret: String,
 });
+
+userSchema.plugin(plm);
 
 module.exports = mongoose.model("user", userSchema);
